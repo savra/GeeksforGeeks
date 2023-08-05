5af00862c2988f528455e16c9c320df5c5aa05d2
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-05 11:33:29 +0300

    Java Hello World

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/JavaHelloWorld.java
@@ -0,0 +1,12 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(name = "Java Hello World", difficulty = Difficulty.SCHOOL,
+url = "https://practice.geeksforgeeks.org/problems/java-hello-world4004/1?page=1&difficulty[]=-2&sortBy=accuracy")
+public class JavaHelloWorld {
+    static void printHelloWorld(){
+        System.out.println("Hello World");
+    }
+}
