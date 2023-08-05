3552e9a213b0d1dfa58df2ed8261bee4e380317a
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-05 09:01:40 +0300

    Initial commit

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/GeeksforGeeksApplication.java
@@ -0,0 +1,13 @@
+package com.hvdbs.savra.geeksforgeeks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeeksforGeeksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeeksforGeeksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/hvdbs/savra/geeksforgeeks/GeeksforGeeksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.hvdbs.savra.geeksforgeeks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GeeksforGeeksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
