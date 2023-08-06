e3e4050903ec87fc1f6c9147558dd28f4a1a8b48
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-06 11:09:21 +0300

    Display longest name

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/DisplayLongestName.java
@@ -0,0 +1,23 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(name = "Display longest name",
+        url = "https://practice.geeksforgeeks.org/problems/display-longest-name0853/1?page=1&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions",
+        difficulty = Difficulty.SCHOOL)
+public class DisplayLongestName {
+    String longest(String names[], int n) {
+        int maxLength = 0;
+        String maxName = "";
+
+        for (int i = 0; i < names.length; i++) {
+            if (names[i].length() > maxLength) {
+                maxLength = names[i].length();
+                maxName = names[i];
+            }
+        }
+
+        return maxName;
+    }
+}
