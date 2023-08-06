8a51e75177949c0b625c40fe3440ed63830bac0b
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-06 09:35:56 +0300

    Print alternate elements of an array

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/PrintAlternateElementsOfAnArray.java
@@ -0,0 +1,16 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(name = "Print alternate elements of an array", difficulty = Difficulty.SCHOOL,
+        url = "https://practice.geeksforgeeks.org/problems/print-alternate-elements-of-an-array/1?page=1&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions")
+public class PrintAlternateElementsOfAnArray {
+    public static void print(int arr[], int n) {
+        for (int i = 0; i < arr.length; i++) {
+            if (i % 2 == 0) {
+                System.out.print(arr[i] + " ");
+            }
+        }
+    }
+}
