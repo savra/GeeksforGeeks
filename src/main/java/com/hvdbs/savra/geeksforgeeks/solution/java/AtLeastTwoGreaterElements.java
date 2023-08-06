1b88aa3ba45ed7138e63388e37a86076078e3961
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-06 12:49:59 +0300

    At least two greater elements

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/AtLeastTwoGreaterElements.java
@@ -0,0 +1,10 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(name = "At least two greater elements",
+        difficulty = Difficulty.SCHOOL,
+        url = "https://practice.geeksforgeeks.org/problems/at-least-two-greater-elements4625/1?page=1&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions")
+public class AtLeastTwoGreaterElements {
+}
