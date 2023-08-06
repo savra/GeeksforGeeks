b8871e495b80e15a7be79fe3eae8e98d37dd0256
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-06 11:25:37 +0300

    Convert String to LowerCase

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/ConvertStringToLowerCase.java
@@ -0,0 +1,12 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(name = "Convert String to LowerCase", difficulty = Difficulty.SCHOOL,
+        url = "https://practice.geeksforgeeks.org/problems/java-convert-string-to-lowercase2313/1?page=1&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions")
+public class ConvertStringToLowerCase {
+    static String toLower(String S) {
+        return S.toLowerCase();
+    }
+}
