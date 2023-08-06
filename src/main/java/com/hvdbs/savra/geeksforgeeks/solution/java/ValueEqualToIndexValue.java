625d8d0d3ce51e57d87d35d0edc40dbf4af039ed
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-06 09:18:50 +0300

    Value equal to index value

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/ValueEqualToIndexValue.java
@@ -0,0 +1,24 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+import java.util.ArrayList;
+
+@GeeksForGeeksInfo(
+        name = "Value equal to index value",
+        url = "https://practice.geeksforgeeks.org/problems/value-equal-to-index-value1330/1?page=1&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions",
+        difficulty = Difficulty.SCHOOL)
+public class ValueEqualToIndexValue {
+    ArrayList<Integer> valueEqualToIndex(int arr[], int n) {
+        ArrayList<Integer> result = new ArrayList<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i + 1 == arr[i]) {
+                result.add(i + 1);
+            }
+        }
+
+        return result;
+    }
+}
