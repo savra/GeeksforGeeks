e6104cff5775c76599e0193a4330d660f251bfb6
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-06 10:51:56 +0300

    Count of smaller elements

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/CountOfSmallerElements.java
@@ -0,0 +1,33 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(name = "Count of smaller elements", difficulty = Difficulty.SCHOOL,
+url = "https://practice.geeksforgeeks.org/problems/count-of-smaller-elements5947/1?page=1&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions")
+public class CountOfSmallerElements {
+    public long countOfElements(long arr[], long n, long x) {
+        int targetElemIndex = -1;
+
+        if (x >= arr[arr.length - 1]) {
+            return arr.length;
+        }
+
+        for (int i = 0; i < arr.length; i++) {
+            if (x >= arr[i]) {
+                targetElemIndex = i;
+                continue;
+            }
+
+            if (targetElemIndex != -1) {
+                return targetElemIndex + 1;
+            }
+        }
+
+        if (targetElemIndex == -1) {
+            targetElemIndex = 0;
+        }
+
+        return targetElemIndex;
+    }
+}
