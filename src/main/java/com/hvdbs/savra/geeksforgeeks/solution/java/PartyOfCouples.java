285270ae812bad88f61a1797a48bbd5388502a9e
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-06 12:18:58 +0300

    Party of Couples

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/PartyOfCouples.java
@@ -0,0 +1,20 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(name = "Party of Couples", difficulty = Difficulty.SCHOOL,
+url = "https://practice.geeksforgeeks.org/problems/alone-in-couple5507/1?page=1&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions")
+public class PartyOfCouples {
+    static int findSingle(int N, int arr[]){
+        java.util.Arrays.sort(arr);
+
+        for (int i = 0; i < arr.length - 1; i += 2) {
+            if (arr[i] != arr[i + 1]) {
+                return arr[i];
+            }
+        }
+
+        return arr[arr.length - 1];
+    }
+}
