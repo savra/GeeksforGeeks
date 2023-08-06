bf1168aa8ddef6fee7a66b8975d34fe211f25c77
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-06 11:39:20 +0300

    Perfect Arrays

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/PerfectArrays.java
@@ -0,0 +1,35 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(name = "Perfect Arrays", difficulty = Difficulty.SCHOOL,
+        url = "https://practice.geeksforgeeks.org/problems/perfect-arrays4645/1?page=1&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions")
+public class PerfectArrays {
+    public boolean IsPerfect(int a[], int n) {
+        if (a.length == 2) {
+            return a[0] == a[1];
+        }
+
+        int x;
+        int y;
+
+        int middleIndx = n / 2;
+
+        if (middleIndx % 2 == 0) {
+            x = middleIndx;
+            y = middleIndx - 1;
+        } else {
+            x = middleIndx;
+            y = middleIndx;
+        }
+
+        for (int i = 0, j = a.length - 1; i < x && j > y; i++, j--) {
+            if (a[i] != a[j]) {
+                return false;
+            }
+        }
+
+        return true;
+    }
+}
