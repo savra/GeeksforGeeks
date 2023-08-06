0ccca582651356462a69596f701225bb405c501e
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-06 09:59:26 +0300

    Arrays (Sum of array)

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/ArraysSumOfArray.java
@@ -0,0 +1,19 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(name = "Arrays (Sum of array)",
+        url = "https://practice.geeksforgeeks.org/problems/c-arrays-sum-of-array-set-14805/1?page=1&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions",
+        difficulty = Difficulty.SCHOOL)
+public class ArraysSumOfArray {
+    public long getSum(long a[], long n) {
+        long result = 0;
+
+        for (long l : a) {
+            result += l;
+        }
+
+        return result;
+    }
+}
--- src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/SumOfArrayElements.java
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/SumOfArrayElements.java
@@ -6,8 +6,7 @@ import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
 @GeeksForGeeksInfo(difficulty = Difficulty.SCHOOL, name = "Sum of Array Elements",
 url = "https://practice.geeksforgeeks.org/problems/sum-of-array-elements2502/1?page=1&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions")
 public class SumOfArrayElements {
-    public static int sumElement(int arr[], int n)
-    {
+    public static int sumElement(int arr[], int n) {
         int result = 0;
 
         for (int i = 0; i < arr.length & i < n; i++) {
