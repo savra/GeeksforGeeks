8c1a4af8798a92c217c68032dfaf34e09156dab9
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-07 17:44:24 +0300

    Reversing the vowels

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/ReversingTheVowels.java
@@ -0,0 +1,30 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
+
+@GeeksForGeeksInfo(name = "Reversing the vowels",
+        difficulty = Difficulty.SCHOOL,
+        url = "https://practice.geeksforgeeks.org/problems/reversing-the-vowels5304/1?page=2&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions")
+public class ReversingTheVowels {
+    String modify (String s) {
+        Set<Character> vowels = new HashSet<>(Arrays.asList('a', 'e', 'i', 'o', 'u'));
+        String reversedVowels = new StringBuilder(s.replaceAll("[^aeiou]", "")).reverse().toString();
+
+        char[] chars = s.toCharArray();
+
+
+        for (int i = 0, j = 0; i < chars.length; i++) {
+            if (vowels.contains(chars[i])) {
+                chars[i] = reversedVowels.charAt(j);
+                j++;
+            }
+        }
+
+        return new String(chars);
+    }
+}
