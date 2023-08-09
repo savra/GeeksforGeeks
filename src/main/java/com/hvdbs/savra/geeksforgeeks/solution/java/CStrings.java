9aafaf457ad4643ad327924f5ac3b434fd01c0f6
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-09 20:46:09 +0300

    C++ Strings

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/CStrings.java
@@ -0,0 +1,14 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.SCHOOL,
+        name = "C++ Strings",
+        url = "https://practice.geeksforgeeks.org/problems/c-strings4609/1?page=2&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions")
+public class CStrings {
+    String conCat(String s1 , String s2) {
+        return s1 + s2;
+    }
+}
