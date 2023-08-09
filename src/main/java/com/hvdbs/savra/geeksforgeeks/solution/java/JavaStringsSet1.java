94cadfcd8f8793f69c8305b77ae7c9b538057752
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-09 20:49:49 +0300

    Java Strings | Set 1

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/JavaStringsSet1.java
@@ -0,0 +1,14 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.SCHOOL,
+        name = "Java Strings | Set 1",
+        url = "https://practice.geeksforgeeks.org/problems/java-strings-set-15112/1?page=2&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions")
+public class JavaStringsSet1 {
+    static String conRevstr(String S1, String S2) {
+        return new StringBuilder(S1 + S2).reverse().toString();
+    }
+}
