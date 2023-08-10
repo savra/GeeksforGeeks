ee04cdb4d5da758ba56c46cb37bd4e42d967fb1a
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-10 08:06:25 +0300

    Delete alternate characters

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/DeleteAlternateCharacters.java
@@ -0,0 +1,22 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.SCHOOL,
+        name = "Delete alternate characters",
+        url = "https://practice.geeksforgeeks.org/problems/java-delete-alternate-characters4036/1?page=2&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions")
+public class DeleteAlternateCharacters {
+    static String delAlternate(String S) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < S.length(); i++) {
+            if (i % 2 == 0) {
+                sb.append(S.charAt(i));
+            }
+        }
+
+        return sb.toString();
+    }
+}
