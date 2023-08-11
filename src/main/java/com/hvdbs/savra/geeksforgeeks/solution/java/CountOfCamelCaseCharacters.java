29cac08765276f095f7376d5987b65da876559e7
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-11 16:02:04 +0300

    Count of camel case characters

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/CountOfCamelCaseCharacters.java
@@ -0,0 +1,22 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.SCHOOL,
+        name = "Count of camel case characters",
+        url = "https://practice.geeksforgeeks.org/problems/find-the-camel3348/1?page=2&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions")
+public class CountOfCamelCaseCharacters {
+    int countCamelCase (String s) {
+        int result = 0;
+
+        for (int i = 0; i < s.length(); i++) {
+            if ((int)s.charAt(i) >= 65 && (int)s.charAt(i) <= 90) {
+                result++;
+            }
+        }
+
+        return result;
+    }
+}
