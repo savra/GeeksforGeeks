4cd14e66bcd63e6296a32cd2136aded4f0b37fc3
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-12 08:58:51 +0300

    Compete the skills

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/CompeteTheSkills.java
@@ -0,0 +1,27 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.SCHOOL,
+        name = "Compete the skills",
+        url = "https://practice.geeksforgeeks.org/problems/compete-the-skills5807/1?page=2&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions"
+)
+public class CompeteTheSkills {
+    public void scores(long a[], long b[]) {
+        int ca = 0;
+        int cb = 0;
+
+        for (int i = 0; i < 3; i++) {
+            if (a[i] > b[i]) {
+                ca++;
+            } else if (a[i] < b[i]) {
+                cb++;
+            }
+        }
+
+        //GFG.ca = ca;
+        //GFG.cb = cb;
+    }
+}
