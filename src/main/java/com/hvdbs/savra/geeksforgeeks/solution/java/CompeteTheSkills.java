9ac91f334b239070679dce9b120b3c5339a5308c
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-12 09:03:56 +0300

    Compete the skills

--- src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/CompeteTheSkills.java
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/CompeteTheSkills.java
@@ -9,6 +9,10 @@ import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
         url = "https://practice.geeksforgeeks.org/problems/compete-the-skills5807/1?page=2&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions"
 )
 public class CompeteTheSkills {
+    //Expected Time Complexity: O(1)
+    //Expected Auxiliary Space: O(1)
+    //My Time Complexity: O(1)
+    //My Auxiliary Space: O(1)
     public void scores(long a[], long b[]) {
         int ca = 0;
         int cb = 0;
