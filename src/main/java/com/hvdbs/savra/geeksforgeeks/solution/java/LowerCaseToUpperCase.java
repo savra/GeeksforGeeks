caa629cbcb3a2e9a21814ac2249882d210d74f77
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-14 06:48:49 +0300

    Lower case to upper case

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/LowerCaseToUpperCase.java
@@ -0,0 +1,15 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.SCHOOL,
+        name = "Lower case to upper case",
+        url = "https://practice.geeksforgeeks.org/problems/lower-case-to-upper-case3410/1?page=2&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions"
+)
+public class LowerCaseToUpperCase {
+    String to_upper(String str) {
+        return str.toUpperCase();
+    }
+}
