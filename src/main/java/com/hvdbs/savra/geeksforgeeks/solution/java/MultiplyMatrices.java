3e8d3c5287bd53ccc0042c53db140bbb3b503f36
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-14 07:29:52 +0300

    Multiply Matrices

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/MultiplyMatrices.java
@@ -0,0 +1,21 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.SCHOOL,
+        name = "Multiply Matrices",
+        url = "https://practice.geeksforgeeks.org/problems/multiply-matrices/1?page=2&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions"
+)
+public class MultiplyMatrices {
+    public static void multiply(int A[][], int B[][], int C[][], int N) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                for (int k = 0; k < N; k++) {
+                    C[i][j] += A[i][k] * B[k][j];
+                }
+            }
+        }
+    }
+}
