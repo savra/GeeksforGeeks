5956e6308bccca63542934a410f73e2bd1136c06
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-15 18:21:47 +0300

    Binary representation

--- src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/BinaryRepresentation.java
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/BinaryRepresentation.java
@@ -9,6 +9,11 @@ import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
         url = "https://practice.geeksforgeeks.org/problems/binary-representation5003/1?page=3&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions"
 )
 public class BinaryRepresentation {
+    //Expected Time Complexity: O(LogN)
+    //Expected Auxiliary Space: O(1)
+
+    //My Time Complexity: O(n)
+    //My Space complexity: O(1)
     static String getBinaryRep(int N){
         return String.format("%30s", Integer.toBinaryString(N)).replace(" ", "0");
     }
