78415f2a504582379d7c0733aeb564e79a99b85b
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-19 08:57:03 +0300

    Program to print reciprocal of letters

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/ProgramToPrintReciprocalOfLetters.java
@@ -0,0 +1,31 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.SCHOOL,
+        name = "Program to print reciprocal of letters",
+        url = "https://practice.geeksforgeeks.org/problems/program-to-print-reciprocal-of-letters36233623/1?page=3&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions"
+)
+public class ProgramToPrintReciprocalOfLetters {
+    static String reciprocalString(String S) {
+        StringBuilder sb = new StringBuilder();
+
+        for (int i = 0; i < S.length(); i++) {
+            char currentChar = S.charAt(i);
+
+            if ((currentChar >= 65 && currentChar <= 90)) {
+                int chrPosition = currentChar - 65;
+                sb.append((char) ('Z' - chrPosition));
+            } else if (currentChar >= 97 && currentChar <= 122) {
+                int chrPosition = currentChar - 97;
+                sb.append((char) ('z' - chrPosition));
+            } else {
+                sb.append(currentChar);
+            }
+        }
+
+        return sb.toString();
+    }
+}
