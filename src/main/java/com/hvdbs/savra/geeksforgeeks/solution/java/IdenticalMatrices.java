0478d943cc90a68fa210e1255f0d056e2d00a11a
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-19 10:11:13 +0300

    Identical Matrices

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/IdenticalMatrices.java
@@ -0,0 +1,23 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.SCHOOL,
+        name = "Identical Matrices",
+        url = "https://practice.geeksforgeeks.org/problems/identical-matrices1042/1?page=3&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions"
+)
+public class IdenticalMatrices {
+    int areMatricesIdentical(int N, int[][] Grid1, int[][] Grid2) {
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < N; j++) {
+                if (Grid1[i][j] != Grid2[i][j]) {
+                    return 0;
+                }
+            }
+        }
+
+        return 1;
+    }
+}
