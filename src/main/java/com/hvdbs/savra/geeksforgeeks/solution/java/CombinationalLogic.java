2908ac0cb99a895a2432daee74401e15eb86b546
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-19 12:51:18 +0300

    Combinational Logic

--- src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/CombinationalLogic.java
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/CombinationalLogic.java
@@ -9,6 +9,8 @@ import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
         url = "https://practice.geeksforgeeks.org/problems/combinational-logic1908/1?page=3&difficulty[]=-2&category[]=Data%20Structures&category[]=Arrays&sortBy=submissions"
 )
 public class CombinationalLogic {
+    //My Time Complexity: O(1)
+    //My Space complexity: O(1)
     static int logicalOperation(int A,int B,int C,int D,int E,int F){
         return (!(A == 1) & (B == 1)) | ((C == 1) & (D == 1)) | ((E == 1) & !(F == 1))? 1 : 0;
     }
