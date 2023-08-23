647d5a590847a1b97d723c1322e77a84d1b64c17
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-23 16:45:25 +0300

    Series AP

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/SeriesAP.java
@@ -0,0 +1,15 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.SCHOOL,
+        name = "Series AP",
+        url = "https://practice.geeksforgeeks.org/problems/series-ap5310/1?page=1&difficulty[]=-2&status[]=unsolved&status[]=attempted&sortBy=submissions"
+)
+public class SeriesAP {
+    int nthTermOfAP(int A1, int A2, int N) {
+        return A1 + (A2 - A1) * (N - 1);
+    }
+}
