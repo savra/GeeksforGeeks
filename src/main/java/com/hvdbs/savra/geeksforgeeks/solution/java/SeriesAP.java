3d3030b5286798c069908fdc520e8139d4bc8c9c
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-23 16:46:54 +0300

    Series AP

--- src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/SeriesAP.java
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/SeriesAP.java
@@ -9,6 +9,8 @@ import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
         url = "https://practice.geeksforgeeks.org/problems/series-ap5310/1?page=1&difficulty[]=-2&status[]=unsolved&status[]=attempted&sortBy=submissions"
 )
 public class SeriesAP {
+    //My Time Complexity: O(1)
+    //My Space complexity: O(1)
     int nthTermOfAP(int A1, int A2, int N) {
         return A1 + (A2 - A1) * (N - 1);
     }
