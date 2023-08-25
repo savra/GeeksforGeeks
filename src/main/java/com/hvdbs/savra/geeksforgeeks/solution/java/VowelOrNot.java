699a1cebb3cb4cc473ac497ba23cd67978df77ad
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-25 18:29:45 +0300

    Vowel or Not

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/VowelOrNot.java
@@ -0,0 +1,15 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.SCHOOL,
+        name = "Vowel or Not",
+        url = "https://practice.geeksforgeeks.org/problems/vowel-or-not0831/1?page=1&difficulty[]=-2&status[]=unsolved&status[]=attempted&sortBy=submissions"
+)
+public class VowelOrNot {
+    String isVowel(char c){
+        return java.util.regex.Pattern.matches("[aeiou]|[AEIOU]", Character.toString(c)) ? "YES" : "NO";
+    }
+}
