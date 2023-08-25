ec14ffa3f0df8a4c321a0ee2c0d17271f3820063
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-25 17:08:49 +0300

    if-else (Decision Making)

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/ifElseDecisionMaking.java
@@ -0,0 +1,23 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.SCHOOL,
+        name = "if-else (Decision Making)",
+        url = "https://practice.geeksforgeeks.org/problems/java-if-else-decision-making0924/1?page=1&difficulty[]=-2&status[]=unsolved&status[]=attempted&sortBy=submissions"
+)
+public class ifElseDecisionMaking {
+    //My Time Complexity: O(1)
+    //My Space complexity: O(1)
+    static String compareNM(int n,int m){
+        if (n == m) {
+            return "equal";
+        } else if (n < m) {
+            return "lesser";
+        } else {
+            return "greater";
+        }
+    }
+}
