aca36bf4a9649ce98b72844b3e6809c711360653
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-25 18:31:02 +0300

    Vowel or Not

--- src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/VowelOrNot.java
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/VowelOrNot.java
@@ -9,6 +9,8 @@ import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
         url = "https://practice.geeksforgeeks.org/problems/vowel-or-not0831/1?page=1&difficulty[]=-2&status[]=unsolved&status[]=attempted&sortBy=submissions"
 )
 public class VowelOrNot {
+    //My Time Complexity: O(1)
+    //My Space complexity: O(1)
     String isVowel(char c){
         return java.util.regex.Pattern.matches("[aeiou]|[AEIOU]", Character.toString(c)) ? "YES" : "NO";
     }
