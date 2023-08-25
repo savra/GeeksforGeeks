9ad678f04f5a91d0afa019d67cf6c315ee68fe8d
Sergey Savrukhin <savra.sv@yandex.ru>
2023-08-25 17:11:08 +0300

    C++ Input / Output

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/CInputOutput.java
@@ -0,0 +1,17 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.SCHOOL,
+        name = "C++ Input / Output",
+        url = "https://practice.geeksforgeeks.org/problems/c-input-output2432/1?page=1&difficulty[]=-2&status[]=unsolved&status[]=attempted&sortBy=submissions"
+)
+public class CInputOutput {
+    //My Time Complexity: O(1)
+    //My Space complexity: O(1)
+    static int multiplication(int A, int B){
+        return A * B;
+    }
+}
