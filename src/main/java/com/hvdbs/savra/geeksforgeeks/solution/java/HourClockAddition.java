4e39846eade841adafcc6f65745e05513e15b5a5
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-01 08:01:05 +0300

    12 hour clock addition

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/HourClockAddition.java
@@ -0,0 +1,17 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.SCHOOL,
+        name = "12 hour clock addition",
+        url = "https://practice.geeksforgeeks.org/problems/12-hour-clock-addition1206/1?page=3&difficulty[]=-2&status[]=unsolved&status[]=attempted&sortBy=submissions"
+)
+public class HourClockAddition {
+    static int clockSum(int num1, int num2){
+        int sum = num1 + num2;
+
+        return sum < 12 ? sum: sum % 12;
+    }
+}
