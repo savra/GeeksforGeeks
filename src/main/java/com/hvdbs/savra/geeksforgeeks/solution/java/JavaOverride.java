f708a9a54800f2896ba8e35a3f2b5e5c37027239
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-03 16:44:26 +0300

    Java Override

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/JavaOverride.java
@@ -0,0 +1,31 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.SCHOOL,
+        name = "Java Override",
+        url = "https://practice.geeksforgeeks.org/problems/java-override/1?page=3&difficulty[]=-2&status[]=unsolved&status[]=attempted&sortBy=submissions"
+)
+public class JavaOverride extends Parent {
+    //My Time Complexity: O(1)
+    //My Space complexity: O(1)
+    @Override
+    void show(int p) { System.out.print(p+" "); }
+
+    @Override
+    void print(int q) {
+        System.out.println(q * q);
+    }
+}
+
+class Parent {
+    void show(int p) {
+        System.out.println(p + " ");
+    }
+
+    void print(int q) {
+        System.out.println(q + " ");
+    }
+}
