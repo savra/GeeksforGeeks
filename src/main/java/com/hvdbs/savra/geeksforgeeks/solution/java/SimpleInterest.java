4c0a0a6ceb027f2bd81ffe520ec9dbeb34b40664
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-08 07:19:49 +0300

    Simple Interest

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/SimpleInterest.java
@@ -0,0 +1,15 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.SCHOOL,
+        name = "Simple Interest",
+        url = "https://practice.geeksforgeeks.org/problems/simple-interest3457/1?page=2&difficulty[]=-2&status[]=unsolved&status[]=attempted&sortBy=submissions"
+)
+public class SimpleInterest {
+    double simpleInterest(int P, int R, int T) {
+        return R / 100.0 * P * T;
+    }
+}
