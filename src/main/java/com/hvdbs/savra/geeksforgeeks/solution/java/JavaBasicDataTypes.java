1dcd686354baa4d6624cb8e2f3bb31e22732b85d
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-16 10:15:50 +0300

    Java Basic Data Types

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/JavaBasicDataTypes.java
@@ -0,0 +1,24 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+import java.util.Scanner;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.SCHOOL,
+        name = "Java Basic Data Types",
+        url = "https://practice.geeksforgeeks.org/problems/java-basic-data-types0041/1?page=2&difficulty[]=-2&status[]=unsolved&status[]=attempted&sortBy=submissions"
+)
+public class JavaBasicDataTypes {
+    int javaIntType(Scanner sc) {
+        return sc.nextInt();
+    }
+
+    String javaStringType(Scanner sc) {
+        return sc.next();
+    }
+
+    float javaFloatType(Scanner sc) {
+        return sc.nextFloat();
+    }
+}
