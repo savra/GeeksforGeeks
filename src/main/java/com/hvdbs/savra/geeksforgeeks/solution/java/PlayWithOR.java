28c49f2e812f2d7d04912c6015d33cafa12854e1
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-17 12:02:30 +0300

    Play With OR

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/PlayWithOR.java
@@ -0,0 +1,26 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.BASIC,
+        name = "Play With OR",
+        url = "https://practice.geeksforgeeks.org/problems/play-with-or5515/1?page=1&difficulty[]=-1&status[]=unsolved&sortBy=accuracy"
+)
+public class PlayWithOR {
+    public static int[] game_with_number (int arr[], int n) {
+        int[] res = new int[n];
+
+        for (int i = 0, j = 1; i < n; i++, j++) {
+            if (j == n) {
+                res[i] = arr[i];
+                break;
+            }
+
+            res[i] = arr[i] | arr[j];
+        }
+
+        return res;
+    }
+}
