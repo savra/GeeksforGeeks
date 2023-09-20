e95966d8573cf4b328c0deebe4c5a5cf7c210b34
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-20 18:59:18 +0300

    Common elements

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/CommonElements.java
@@ -0,0 +1,56 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+import java.util.ArrayList;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.EASY,
+        name = "Common elements",
+        url = "https://practice.geeksforgeeks.org/problems/common-elements1132/1?utm_source=gfg&utm_medium=article&utm_campaign=bottom_sticky_on_article"
+)
+public class CommonElements {
+    //My Time Complexity: O(n)
+    //My Space complexity: O(n)
+    ArrayList<Integer> commonElements(int A[], int B[], int C[], int n1, int n2, int n3) {
+        int i = 0;
+        int j = 0;
+        int k = 0;
+
+        ArrayList<Integer> res = new ArrayList<>();
+
+        while(i < n1 && j < n2 && k < n3) {
+            if (A[i] < B[j]) {
+                i++;
+            } else if (A[i] > B[j]) {
+                j++;
+            } else {
+                if (A[i] < C[k]) {
+                    i++;
+                } else if (A[i] > C[k]) {
+                    k++;
+                } else {
+                    boolean existsInResult = false;
+
+                    for (Integer item : res) {
+                        if (item == A[i]) {
+                            existsInResult = true;
+                            break;
+                        }
+                    }
+
+                    if (!existsInResult) {
+                        res.add(A[i]);
+                    }
+
+                    i++;
+                    j++;
+                    k++;
+                }
+            }
+        }
+
+        return res;
+    }
+}
