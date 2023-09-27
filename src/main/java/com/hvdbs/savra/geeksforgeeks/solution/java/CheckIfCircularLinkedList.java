12b9fc1d42bdafd536041b5262a68ab29b592034
Sergey Savrukhin <savra.sv@yandex.ru>
2023-09-27 07:16:42 +0300

    Check If Circular Linked List

--- src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/CheckIfCircularLinkedList.java
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/CheckIfCircularLinkedList.java
@@ -19,6 +19,8 @@ public class CheckIfCircularLinkedList {
         }
     }
 
+    //My Time Complexity: O(n)
+    //My Space complexity: O(1)
     boolean isCircular(Node head) {
         if (head == null) {
             return true;
