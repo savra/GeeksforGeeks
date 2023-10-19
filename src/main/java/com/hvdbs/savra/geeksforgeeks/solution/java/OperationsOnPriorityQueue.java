b1d2f7d165c2e49997c982185cf3910aef1de720
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-19 17:31:48 +0300

    Operations on PriorityQueue

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/OperationsOnPriorityQueue.java
@@ -0,0 +1,28 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+import java.util.PriorityQueue;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.BASIC,
+        name = "Operations on PriorityQueue",
+        url = "https://practice.geeksforgeeks.org/problems/operations-on-priorityqueue/1?page=1&difficulty[]=0&status[]=unsolved&category[]=Queue&sortBy=accuracy"
+)
+public class OperationsOnPriorityQueue {
+    // Function to insert element into the queue
+    static void insert(PriorityQueue<Integer> q, int k) {
+        q.offer(k);
+    }
+
+    // Function to find an element k
+    static boolean find(PriorityQueue<Integer> q, int k) {
+        return q.contains(k);
+    }
+
+    // Function to delete the max element from queue
+    static int delete(PriorityQueue<Integer> q) {
+        return q.poll();
+    }
+}
