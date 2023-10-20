5e9d88288c05a62d6c3fb8de685e33c624b7fab6
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-20 14:48:24 +0300

    Generate Binary Numbers

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/GenerateBinaryNumbers.java
@@ -0,0 +1,23 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+import java.util.ArrayList;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.EASY,
+        name = "Generate Binary Numbers",
+        url = "https://practice.geeksforgeeks.org/problems/generate-binary-numbers-1587115620/1?page=1&difficulty[]=0&status[]=unsolved&category[]=Queue&sortBy=accuracy"
+)
+public class GenerateBinaryNumbers {
+    static ArrayList<String> generate(int N) {
+        ArrayList<String> res = new ArrayList<>();
+
+        for (int i = 1; i <= N; i++) {
+            res.add(Integer.toBinaryString(i));
+        }
+
+        return res;
+    }
+}
