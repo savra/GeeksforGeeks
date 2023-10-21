da54363e9b71c70dc0bb187d1e62eeeabdd4651c
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-21 08:01:14 +0300

    Preorder Traversal

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/PreorderTraversal.java
@@ -0,0 +1,67 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+import java.util.ArrayDeque;
+import java.util.ArrayList;
+import java.util.Deque;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.BASIC,
+        name = "Preorder Traversal",
+        url = "https://practice.geeksforgeeks.org/problems/preorder-traversal/1?page=1&difficulty[]=-1&status[]=unsolved&category[]=Stack&sortBy=accuracy"
+)
+public class PreorderTraversal {
+    class Node {
+        int data;
+        Node left, right;
+
+        Node(int d) {
+            data = d;
+            left = right = null;
+        }
+    }
+
+    //iterative
+    static ArrayList<Integer> preorder(Node root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        Deque<Node> s = new ArrayDeque<>();
+
+        while (root != null || !s.isEmpty()) {
+            if (!s.isEmpty()) {
+                root = s.pollFirst();
+            }
+
+            while (root != null) {
+                res.add(root.data);
+
+                if (root.right != null) {
+                    s.offerFirst(root.right);
+                }
+
+                root = root.left;
+            }
+        }
+
+        return res;
+    }
+
+    //recursive
+  /*  static ArrayList<Integer> preorder(Node root) {
+        ArrayList<Integer> res = new ArrayList<>();
+        traverse(root, res);
+
+        return res;
+    }*/
+
+ /*   static void traverse(Node root, List<Integer> res) {
+        if (root == null) {
+            return;
+        }
+
+        res.add(root.data);
+        traverse(root.left, res);
+        traverse(root.right, res);
+    }*/
+}
