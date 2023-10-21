4493a2d756c274404421403ac00cb652ca9b830b
Sergey Savrukhin <savra.sv@yandex.ru>
2023-10-21 11:17:58 +0300

    Queue using two Stacks

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/QueueUsingTwoStacks.java
@@ -0,0 +1,39 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+import java.util.Stack;
+
+@GeeksForGeeksInfo(
+        difficulty = Difficulty.EASY,
+        name = "Queue using two Stacks",
+        url = "https://practice.geeksforgeeks.org/problems/queue-using-two-stacks/1?page=1&difficulty[]=0&status[]=unsolved&category[]=Queue&sortBy=accuracy"
+)
+public class QueueUsingTwoStacks {
+    Stack<Integer> s1 = new Stack<Integer>();
+    Stack<Integer> s2 = new Stack<Integer>();
+
+    //Function to push an element in queue by using 2 stacks.
+    void Push(int x) {
+        s1.push(x);
+    }
+
+
+    //Function to pop an element from queue by using 2 stacks.
+    int Pop() {
+        if (s1.isEmpty() && s2.isEmpty()) {
+            return -1;
+        }
+
+        if (!s2.isEmpty()) {
+            return s2.pop();
+        }
+
+        while (!s1.isEmpty()) {
+            s2.push(s1.pop());
+        }
+
+        return s2.pop();
+    }
+}
