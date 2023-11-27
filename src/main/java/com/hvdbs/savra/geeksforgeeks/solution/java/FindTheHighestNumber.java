4ad895a92c652c7fed9dcfdcf39297e50d7d3c15
Sergey Savrukhin <savra.sv@yandex.ru>
2023-11-27 19:12:18 +0300

    Find the Highest number

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/FindTheHighestNumber.java
@@ -0,0 +1,29 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.GeeksForGeeksInfo;
+import com.hvdbs.savra.geeksforgeeks.statsgenerator.enums.Difficulty;
+
+import java.util.List;
+
+@GeeksForGeeksInfo(
+        name = "Find the Highest number",
+        difficulty = Difficulty.EASY,
+        url = "https://practice.geeksforgeeks.org/problems/find-the-highest-number2259/1?page=1&category=Binary%20Search&difficulty=School,Basic,Easy&status=unsolved&sortBy=accuracy")
+public class FindTheHighestNumber {
+    public int findPeakElement(List<Integer> a) {
+        int l = 0;
+        int r = a.size() - 1;
+
+        while (l < r) {
+            int mid = l + (r - l) / 2;
+
+            if (a.get(mid) < a.get(mid + 1)) {
+                l = mid + 1;
+            } else {
+                r = mid;
+            }
+        }
+
+        return a.get(l);
+    }
+}
