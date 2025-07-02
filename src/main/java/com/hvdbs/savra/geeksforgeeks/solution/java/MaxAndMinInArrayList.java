d56f4db92e66e82a537c2fd78a25903b1bcd4056
Sergey Savrukhin <savra.sv@yandex.ru>
2025-07-02 21:08:19 +0300

    Max and Min In ArrayList

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/MaxAndMinInArrayList.java
@@ -0,0 +1,29 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.statsgenerator.CodeInfo;
+import com.hvdbs.savra.statsgenerator.enums.Complexity;
+import com.hvdbs.savra.statsgenerator.enums.Difficulty;
+
+import java.util.ArrayList;
+
+@CodeInfo(
+        difficulty = Difficulty.BASIC,
+        spaceComplexity = Complexity.ConstantComplexity.CONSTANT,
+        timeComplexity = Complexity.ConstantComplexity.LINEAR,
+        url = "https://www.geeksforgeeks.org/problems/max-and-min-in-arraylist/1?page=1&category=Arrays,Java&difficulty=Basic&status=unsolved&sortBy=accuracy",
+        name = "Max and Min In ArrayList")
+public class MaxAndMinInArrayList {
+    public static int maximumElement(ArrayList<Integer> arr) {
+        return arr.stream()
+                .mapToInt(value -> value.intValue())
+                .max()
+                .getAsInt();
+    }
+
+    public static int minimumElement(ArrayList<Integer> arr) {
+        return arr.stream()
+                .mapToInt(value -> value.intValue())
+                .min()
+                .getAsInt();
+    }
+}
