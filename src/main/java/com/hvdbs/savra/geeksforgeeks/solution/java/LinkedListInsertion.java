9aac50b4d2cb133e23143689ae93406b4827bcba
Sergey Savrukhin <savra.sv@yandex.ru>
2025-07-13 19:31:54 +0300

    LinkedList Insertion

--- src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/LinkedListInsertion.java
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/LinkedListInsertion.java
@@ -1,45 +1,26 @@
 package com.hvdbs.savra.geeksforgeeks.solution.java;
 
 import com.hvdbs.savra.statsgenerator.CodeInfo;
+import com.hvdbs.savra.statsgenerator.enums.Complexity;
 import com.hvdbs.savra.statsgenerator.enums.Difficulty;
+
+import java.util.LinkedList;
 
 @CodeInfo(
         difficulty = Difficulty.BASIC,
-        name = "Linked List Insertion",
-        url = "https://practice.geeksforgeeks.org/problems/linked-list-insertion-1587115620/1?page=1&difficulty[]=-1&status[]=unsolved&category[]=Linked%20List&sortBy=accuracy"
+        name = "LinkedList Insertion",
+        url = "https://www.geeksforgeeks.org/problems/linkedlist-insertion/1?page=1&category=Arrays,Java&difficulty=Basic&status=unsolved&sortBy=accuracy",
+        spaceComplexity = Complexity.ConstantComplexity.LINEAR,
+        timeComplexity = Complexity.ConstantComplexity.LINEAR
 )
 public class LinkedListInsertion {
-    class Node {
-        int data;
-        Node next;
+    public static LinkedList<Integer> insertion(int[] arr) {
+        LinkedList<Integer> result = new LinkedList<>();
 
-        Node(int x) {
-            data = x;
-            next = null;
-        }
-    }
-
-    Node insertAtBeginning(Node head, int x) {
-        Node newNode = new Node(x);
-
-        newNode.next = head;
-
-        return newNode;
-    }
-
-    //Function to insert a node at the end of the linked list.
-    Node insertAtEnd(Node head, int x) {
-        if (head == null) {
-            return insertAtBeginning(head, x);
+        for (int i : arr) {
+            result.add(i);
         }
 
-        Node cur = head;
-
-        for (; cur.next != null; cur = cur.next) {
-        }
-
-        cur.next = new Node(x);
-
-        return head;
+        return result;
     }
 }
