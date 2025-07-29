8d5c27daf4a5496b9a1164fbbd0a380323ea53b5
Sergey Savrukhin <savra.sv@yandex.ru>
2025-07-29 21:54:52 +0300

    Sorted Frequency

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/SortedFrequency.java
@@ -0,0 +1,29 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.statsgenerator.CodeInfo;
+import com.hvdbs.savra.statsgenerator.enums.Complexity;
+import com.hvdbs.savra.statsgenerator.enums.Difficulty;
+
+import java.util.Map;
+import java.util.TreeMap;
+
+@CodeInfo(
+        name = "Sorted Frequency",
+        url = "https://www.geeksforgeeks.org/problems/sorted-frequency/1?page=1&category=Arrays,Java&difficulty=Basic&status=unsolved&sortBy=accuracy",
+        timeComplexity = Complexity.ConstantComplexity.LINEARITHMIC,
+        spaceComplexity = Complexity.ConstantComplexity.LINEAR,
+        difficulty = Difficulty.BASIC
+)
+public class SortedFrequency {
+    public static void freqSorted(int arr[]) {
+        Map<Integer, Integer> result = new TreeMap<>();
+
+        for (int i = 0; i < arr.length; i++) {
+            result.put(arr[i], result.getOrDefault(arr[i], 0) + 1);
+        }
+
+        for (Map.Entry e : result.entrySet()) {
+            System.out.println(e.getKey() + " " + e.getValue());
+        }
+    }
+}
