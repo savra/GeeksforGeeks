d719fe131796133cf033fc374c6e0a709733ae09
Sergey Savrukhin <savra.sv@yandex.ru>
2025-08-12 23:22:37 +0300

    Function With No Arguments

---
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/FunctionWithNoArguments.java
@@ -0,0 +1,18 @@
+package com.hvdbs.savra.geeksforgeeks.solution.java;
+
+import com.hvdbs.savra.statsgenerator.CodeInfo;
+import com.hvdbs.savra.statsgenerator.enums.Complexity;
+import com.hvdbs.savra.statsgenerator.enums.Difficulty;
+
+@CodeInfo(
+        name = "Function With No Arguments",
+        url = "https://www.geeksforgeeks.org/problems/function-with-no-arguments-1605703188/1?page=1&category=Arrays,Java&difficulty=Basic&status=unsolved&sortBy=accuracy",
+        difficulty = Difficulty.BASIC,
+        spaceComplexity = Complexity.ConstantComplexity.CONSTANT,
+        timeComplexity = Complexity.ConstantComplexity.CONSTANT
+)
+public class FunctionWithNoArguments {
+    public void helloFunction() {
+        System.out.println("Hello");
+    }
+}
