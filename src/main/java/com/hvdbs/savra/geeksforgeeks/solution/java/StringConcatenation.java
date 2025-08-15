b30911b5e9a963799c3671deb4f2f9fb706c6ea6
Sergey Savrukhin <savra.sv@yandex.ru>
2025-08-16 00:00:11 +0300

    String Concatenation

--- src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/StringConcatenation.java
+++ src/main/java/com/hvdbs/savra/geeksforgeeks/solution/java/StringConcatenation.java
@@ -1,4 +1,20 @@
 package com.hvdbs.savra.geeksforgeeks.solution.java;
 
+import com.hvdbs.savra.statsgenerator.CodeInfo;
+import com.hvdbs.savra.statsgenerator.enums.Complexity;
+import com.hvdbs.savra.statsgenerator.enums.Difficulty;
+
+import java.util.ArrayList;
+
+@CodeInfo(
+        url = "https://www.geeksforgeeks.org/problems/string-concatenation/1?page=1&category=Arrays,Java&difficulty=Basic&status=unsolved&sortBy=accuracy",
+        difficulty = Difficulty.BASIC,
+        spaceComplexity = Complexity.ConstantComplexity.LINEAR,
+        timeComplexity = Complexity.ConstantComplexity.LINEAR,
+        name = "String Concatenation"
+)
 public class StringConcatenation {
+    public static String concatenateStrings(ArrayList<String> arr) {
+        return String.join("", arr);
+    }
 }
